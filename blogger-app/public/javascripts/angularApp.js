a6497537f8711828417940a3422796b9a75e0ba9
rakesh maddala <rakesh.venus@gmail.com>
2016-06-22 07:58:13 +0530

    Added Blog UI Changes

--- blogger-app/public/javascripts/angularApp.js
+++ blogger-app/public/javascripts/angularApp.js
@@ -1,37 +1,69 @@
 var loremIpsumTitle = "Lorem Ipsum Title";
 var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
 var myBlogApplication = angular.module("blog-app",[]) ;
 
 myBlogApplication.controller('MainBlogCtrl', ['$scope',function($scope){
   $scope.title = 'Blog Application!';
-  $scope.posts = $scope.posts = [
-					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum},
-					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum}
+  $scope.posts =    [
+					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Politics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Economics', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Pharmaceuticals', postContent: loremIpsum, editmode: false},
+					  {title: loremIpsumTitle, category: 'Agriculture', postContent: loremIpsum, editmode: false}
 					];
+  $scope.tempPosts = $scope.posts ;
   $scope.allBlogCount = $scope.posts.length;
-  $scope.politicsCount = 5 ;
-  $scope.economicsCount = 5 ;
-  $scope.agriCount = 5 ;
-  $scope.pharmaCount = 5 ;
-  $scope.toggleEdits = false;
+  $scope.politicsCount = $scope.posts.filter(function(el){
+						      return el.category === "Politics"
+						 }).length ; 
+  $scope.economicsCount = $scope.posts.filter(function(el){
+						      return el.category === "Economics"
+						 }).length  ;
+  $scope.agriCount =  $scope.posts.filter(function(el){
+						      return el.category === "Agriculture"
+						 }).length  ;
+  $scope.pharmaCount = $scope.posts.filter(function(el){
+						      return el.category === "Pharmaceuticals"
+						 }).length  ;
+  
+  $scope.makeBlogEditable = function(currEvent,postIndex){
+	$scope.posts[postIndex].editmode = true;
+	currEvent.currentTarget.previousElementSibling.previousElementSibling.setAttribute('contentEditable','true') ;
+	currEvent.currentTarget.previousElementSibling.previousElementSibling.focus();
+  };
+  
+  $scope.submitBlogChanges = function(currEvent,postIndex){
+	currEvent.currentTarget.previousElementSibling.previousElementSibling.previousElementSibling.setAttribute('contentEditable','false');
+	$scope.posts[postIndex].editmode = false;
+  };
+  
+  $scope.filterBlogs = function(blogCategory, currEvent){
+	  $scope.posts = $scope.tempPosts.filter(function(el){
+								if(blogCategory === "All"){
+									return true ;
+								}else{
+									return el.category === blogCategory ;
+								}
+						 }) ;
+	  $(currEvent.target).parent().siblings().removeClass("active");
+	  $(currEvent.target).parent().addClass("active") ;
+  }
   
   $scope.addPost = function(){
 	  $scope.posts.push({title: 'A new post!', category: 'Politics', postContent: loremIpsum});
@@ -39,18 +71,6 @@ myBlogApplication.controller('MainBlogCtrl', ['$scope',function($scope){
   
   $scope.addPostDialog = function(){
 	  $scope.posts.push({title: 'A new post!', category: 'Politics', postContent: loremIpsum});
-  };
-  
-  $scope.makeBlogEditable = function(currEvent){
-	currEvent.currentTarget.previousElementSibling.previousElementSibling.setAttribute('contentEditable','true')
-	currEvent.currentTarget.nextElementSibling.setAttribute('ng-show','true') ;
-	currEvent.currentTarget.setAttribute('ng-show','false') ;
-  };
-  
-  $scope.submitBlogChanges = function(currEvent){
-	currEvent.currentTarget.previousElementSibling.previousElementSibling.previousElementSibling.setAttribute('contentEditable','false');
-	currEvent.currentTarget.previousElementSibling.setAttribute('ng-show','true') ;
-	currEvent.currentTarget.setAttribute('ng-show','false') ;
   };
   
 }]);
